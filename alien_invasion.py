40128bf2a8638ae17e5c42e63ec71cdb67c68cc0
lichen <1725350830@qq.com>
2025-07-01 22:54:06 +0800

    Add ship class and image, implement ship rendering in game

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from setting import Settings
+from ship import Ship
 
 class AlienInvasion:
     def __init__(self):
@@ -12,6 +13,8 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         while True:
             for event in pygame.event.get():
@@ -19,6 +22,8 @@ class AlienInvasion:
                     sys.exit()
 
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+            
             pygame.display.flip()
 
 if __name__ == '__main__':
