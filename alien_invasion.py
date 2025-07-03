37a1e8ae0a88394b8c8df636a97fbe2b9b554626
lichen <1725350830@qq.com>
2025-07-04 00:03:19 +0800

    Refactor event handling to use match-case syntax for improved readability

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,37 +23,41 @@ class AlienInvasion:
 
     def _check_events(self):
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                self._check_keydown_events(event)
-            elif event.type == pygame.KEYUP:
-                self._check_keyup_events(event)
+            match event.type:
+                case pygame.QUIT:
+                    sys.exit()
+                case pygame.KEYDOWN:
+                    self._check_keydown_events(event)
+                case pygame.KEYUP:
+                    self._check_keyup_events(event)
 
     def _check_keydown_events(self, event):
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = True
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = True
-        elif event.key == pygame.K_UP:
-            self.ship.moving_up = True
-        elif event.key == pygame.K_DOWN:
-            self.ship.moving_down = True
-        elif event.key == pygame.K_q:
-            sys.exit()
+        match event.key:
+            case pygame.K_RIGHT:
+                self.ship.moving_right = True
+            case pygame.K_LEFT:
+                self.ship.moving_left = True
+            case pygame.K_UP:
+                self.ship.moving_up = True
+            case pygame.K_DOWN:
+                self.ship.moving_down = True
+            case pygame.K_q:
+                sys.exit()
     
 
     def _check_keyup_events(self, event):
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = False
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = False
-        elif event.key == pygame.K_UP:
-            self.ship.moving_up = False
-        elif event.key == pygame.K_DOWN:
-            self.ship.moving_down = False
-        elif event.key == pygame.K_q:
-             sys.exit()
+        match event.key:
+            case pygame.K_RIGHT:
+                self.ship.moving_right = False
+            case pygame.K_LEFT:
+                self.ship.moving_left = False
+            case pygame.K_UP:
+                self.ship.moving_up = False
+            case pygame.K_DOWN:
+                self.ship.moving_down = False
+            case pygame.K_q:
+                sys.exit()
+                
 
 
     def _update_screen(self):
