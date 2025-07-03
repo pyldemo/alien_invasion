da585842689f3fb98eab1a9a9d90d51b4faeea55
lichen <1725350830@qq.com>
2025-07-03 23:41:06 +0800

    Enhance event handling to ensure consistent exit behavior on 'q' key press

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,13 +35,17 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
     
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
+            self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+            self.ship.moving_left = False
+        elif event.key == pygame.K_q:
+             sys.exit()
 
 
     def _update_screen(self):
