7a1cbb8fa01e52dcde68353f0558360e1125f82a
lichen <1725350830@qq.com>
2025-07-09 23:38:27 +0800

    Add game over logic and update game state handling

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,9 +27,12 @@ class AlienInvasion:
     def run_game(self):
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+            
+            if self.stats.game_active:    
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+                
             self._update_screen()
 
     def _check_events(self):
@@ -106,16 +109,18 @@ class AlienInvasion:
                 break
 
     def _ship_hit(self):
-        self.stats.ships_left -= 1
+        if self.stats.ships_left > 0:
+            self.stats.ships_left -= 1 
         
-        self.aliens.empty()
-        self.bullets.empty()
-        
-        self._create_fleet()
-        self.ship.center_ship()
-        
-        sleep(0.5)
-        
+            self.aliens.empty()
+            self.bullets.empty()
+            
+            self._create_fleet()
+            self.ship.center_ship()
+            
+            sleep(0.5)
+        else:
+            self.stats.game_active = False
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
--- game_stats.py
+++ game_stats.py
@@ -2,6 +2,8 @@ class GameStats:
     def __init__(self, game):
         self.settings = game.settings
         self.reset_stats()
-        
+
+        self.game_active = True
+
     def reset_stats(self):
         self.ships_left = self.settings.ship_limit
